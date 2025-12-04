06fd0d2d8903410620b65c36a9a7ae04888185c8
fgirlanda <f.girlanda22@gmail.com>
2025-12-04 22:28:53 +0100

    fine giorno 1 setup giorno 2

--- 2025/day1/day1.py
+++ 2025/day1/day1.py
@@ -10,12 +10,89 @@ def main():
             direction = riga[0]
             valore = int(riga[1:])
             istruzioni.append((direction, valore))
-        
-    result = parte1(istruzioni)
-    print(result)
-
-def parte1(istruzioni):
+    
     start = 50
+    result1 = parte1(istruzioni, start)
+    result2 = parte2(istruzioni, start)
+    result2_alt = parte2_alt(istruzioni, start)
+    print("Parte 1 : ", result1, 
+          "\nParte 2:", result2,
+          "\nParte 2 alt:", result2_alt)
+    
+def parte1(istruzioni, start):
+    result = 0
+    temp = start
+    for istruzione in istruzioni:
+        if istruzione[0] == 'L':
+            temp -= istruzione[1]
+        else:
+            temp += istruzione[1]
+        temp = temp%100
+        if temp == 0:
+            result += 1
+            
+    return result
+
+def parte2_alt(istruzioni, start):
+    # PROBLEMA = non conta passaggi multipli dallo 0 + 
+    # result = 0
+    # temp = start
+    # prev = temp
+    # for istruzione in istruzioni:
+    #     if istruzione[0] == 'L':
+    #         temp -= istruzione[1]
+    #     else:
+    #         temp += istruzione[1]
+            
+        
+    #     if temp != temp%100 and prev != 0:
+    #         result += 1
+        
+    #     temp = temp%100
+    #     prev = temp
+
+    # return result
+    
+    # VERSIONE CORRETTA
+    result = 0
+    temp = start
+    
+    for istruzione in istruzioni:
+        if istruzione[0] == 'L':
+            nuovo = temp - istruzione[1]
+            # Attraversamenti andando a sinistra
+            result += (temp - 1) // 100 - (nuovo - 1) // 100
+        else:
+            nuovo = temp + istruzione[1]
+            # Attraversamenti andando a destra
+            result += nuovo // 100 - temp // 100
+        
+        temp = nuovo % 100
+            
+    return result
+    
+    for istruzione in istruzioni:
+        if istruzione[0] == 'L':
+            nuovo = temp - istruzione[1]
+        else:
+            nuovo = temp + istruzione[1]
+        
+        # Conta quante volte attraversiamo lo 0
+        if nuovo > temp:  # Movimento verso destra (R)
+            result += (nuovo // 100) - (temp // 100)
+        elif nuovo < temp:  # Movimento verso sinistra (L)
+            # Conta quanti multipli di 100 attraversiamo scendendo
+            # Formula simmetrica: da temp scendiamo a nuovo
+            # Attraversiamo 0, -100, -200... 
+            # Usiamo ceil per temp e floor per nuovo
+            result += (temp - 1) // 100 - (nuovo - 1) // 100
+        
+        temp = nuovo % 100
+
+    return result
+    
+
+def parte2(istruzioni, start):
     result = 0
     temp = start
     for istruzione in istruzioni:
@@ -23,18 +100,19 @@ def parte1(istruzioni):
             for _ in range(istruzione[1]):
                 if temp == 0:
                     temp = 99
+                    if _ != 0: result += 1
                     continue
                 temp -= 1
         else:
             for _ in range(istruzione[1]):
                 if temp == 99:
                     temp = 0
+                    result += 1
                     continue
                 temp += 1
-        if temp == 0:
-                result += 1
             
     return result
+
                 
             
                 
---
+++ 2025/day2/day2.py
@@ -0,0 +1,10 @@
+# Script per Advent of Code 2025 - giorno 2
+
+def main():
+    FILE_PATH = '2025/day2/esempio.txt'
+
+    with open(FILE_PATH) as file:
+        pass
+
+if __name__ == "__main__":
+    main()
